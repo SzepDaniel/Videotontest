d9ed0327466118c50b5a1dab147447059c993b3e
szedan20 <datidusz1234@gmail.com>
2025-08-08 15:58:06 +0200

    Videoton

---
+++ videotontest/videotontest/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GetIDApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(13, 13);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(775, 268);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 329);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(195, 87);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Indítás";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(302, 329);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(195, 87);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Leállítás";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(593, 329);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(195, 87);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Mentés";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ videotontest/videotontest/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using GetID;  // A DLL namespace-e
+
+namespace GetIDApp
+{
+    public partial class Form1 : Form
+    {
+        private GetID.GetID generator;
+
+        public Form1()
+        {
+            InitializeComponent();
+            generator = new GetID.GetID();
+
+            generator.ValueChanged += Generator_ValueChanged;
+            generator.ErrorChanged += Generator_ErrorChanged;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                generator.Go();
+                LogMessage("Generálás elindítva...");
+            }
+            catch (Exception ex)
+            {
+                LogMessage("Hiba az indításkor: " + ex.Message);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                generator.Stop();
+                LogMessage("Generálás leállítva.");
+            }
+            catch (Exception ex)
+            {
+                LogMessage("Hiba a leállításkor: " + ex.Message);
+            }
+        }
+
+        private void Generator_ValueChanged(object sender, EventArgs e)
+        {
+            string value = generator.Value;
+
+            // UI frissítés biztonságosan
+            if (InvokeRequired)
+            {
+                BeginInvoke((Action)(() => ProcessValue(value)));
+            }
+            else
+            {
+                ProcessValue(value);
+            }
+        }
+
+        private void Generator_ErrorChanged(object sender, EventArgs e)
+        {
+            string error = generator.ErrorMessage;
+
+            if (InvokeRequired)
+            {
+                BeginInvoke((Action)(() => LogMessage("Hiba: " + error)));
+            }
+            else
+            {
+                LogMessage("Hiba: " + error);
+            }
+        }
+
+        private void ProcessValue(string value)
+        {
+            string result = $"{value} - ";
+
+            if (Regex.IsMatch(value, @"^[A-Z][0-9]{4}$"))
+            {
+                result += "MEGFELELŐ";
+            }
+            else
+            {
+                result += "NEM MEGFELELŐ";
+            }
+
+            LogMessage(result);
+        }
+
+        private void LogMessage(string message)
+        {
+            txtOutput.AppendText(message + Environment.NewLine);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Szöveges fájl (*.txt)|*.txt",
+                Title = "Mentés fájlba"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, txtOutput.Text);
+                    MessageBox.Show("Mentés sikeres!", "Mentés", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hiba a mentés során: " + ex.Message, "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
